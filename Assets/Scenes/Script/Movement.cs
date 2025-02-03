28c3964754d7afb27254c562cf0517d43cdc1791
Noyasumi <angelsuryadiii.com>
2025-02-03 12:44:18 +0700

    Updated Chara Movement

--- Assets/Scenes/Script/Movement.cs
+++ Assets/Scenes/Script/Movement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb2d;
+    private float direction;
+    private float speed = 8f;
+
+    private void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        if (rb2d != null)
+        {
+            gameObject.AddComponent<Rigidbody2D>();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdatePosition();
+        SetDirection(Input.GetAxisRaw("Horizontal");
+    }
+
+    private void UpdatePosition()
+    {
+        Vector2 velocity = rb2d.velocity;
+        velocity.x = direction * speed;
+        rb2d.velocity = velocity;
+    }
+
+    private void SetDirection(float direction)
+    {
+        this.direction = direction;
     }
 }
